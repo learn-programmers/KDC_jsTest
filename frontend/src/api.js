ccd71783e5700476db33fd6c7aec98044809ba0a
hyer0705 <hyer1205@gmail.com>
2023-08-03 16:53:38 +0900

    Update API_ENDPOINT amazonaws to localhost

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,10 +1,10 @@
-const API_ENDPOINT =
-  "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev";
+const API_ENDPOINT = "http://localhost:4001";
+// "https://rhdd0roxs5.execute-api.ap-northeast-2.amazonaws.com/dev";
 
 const api = {
-  fetchCats: keyword => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then(res =>
+  fetchCats: (keyword) => {
+    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) =>
       res.json()
     );
-  }
+  },
 };
